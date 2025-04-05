b0388cd454f0f81099d5ade911728f03bb1128bd
Alex Khavin <alexandr_havin@yahoo.com>
2025-04-05 20:15:33 +0300

    ready projects section

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,34 +1,52 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
-//   loop: true, безперервна прокрутка
   initialSlide: 0,
   spaceBetween: 32,
   slidesPerView: "auto",
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
   navigation: {
     nextEl: '#btn-next',
     prevEl: '#btn-prev',
   },
+});
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+function updateNavBtn() {
+  const btnPrev = document.getElementById('btn-prev');
+  const btnNext = document.getElementById('btn-next');
+
+  if (swiper.isBeginning) {
+    btnPrev.disabled = true;
+    btnPrev.classList.add('disabled');
+  } else {
+    btnPrev.disabled = false;
+    btnPrev.classList.remove('disabled');
+  }
+
+  if (swiper.isEnd) {
+    btnNext.disabled = true;
+    btnNext.classList.add('disabled');
+  } else {
+    btnNext.disabled = false;
+    btnNext.classList.remove('disabled');
+  }
+}
+
+updateNavBtn();
+
+swiper.on('slideChange', updateNavBtn);
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowRight' && !swiper.isEnd) {
+    swiper.slideNext();
+  } else if (event.key === 'ArrowLeft' && !swiper.isBeginning) {
+    swiper.slidePrev();
+  }
 });
 
 document.getElementById('btn-next').addEventListener('click', () => {
