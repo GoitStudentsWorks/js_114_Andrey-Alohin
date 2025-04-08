b0f82d47e8d523c309322afa58da8a24946e4c13
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-04-08 19:24:26 +0300

    fix js

--- src/js/about.js
+++ src/js/about.js
@@ -4,6 +4,8 @@ import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import { Navigation, Keyboard } from 'swiper/modules';
+
 const aboutBtnRefs = document.querySelector('#about-btn-js');
 const aboutRefs = document.querySelector('#swiper-about');
 
@@ -11,11 +13,21 @@ const aboutAccordion = new Accordion('.accordion-container');
 aboutAccordion.open(0);
 
 const aboutSwiper = new Swiper('#swiper-about', {
+  modules: [Navigation, Keyboard],
   direction: 'horizontal',
   loop: true,
   initialSlide: 0,
   spaceBetween: 0,
   allowSlidePrev: false,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  navigation: {
+    nextEl: aboutBtnRefs,
+  },
 
   breakpoints: {
     0: {
@@ -32,23 +44,15 @@ const aboutSwiper = new Swiper('#swiper-about', {
 
 const observAbout = new IntersectionObserver(entries => {
   if (entries[0].isIntersecting) {
-    aboutBtnRefs.addEventListener('click', slideNext);
-    document.addEventListener('keydown', nextSwap);
+    aboutBtnRefs.addEventListener('focus', nextSwap);
   } else {
-    aboutBtnRefs.removeEventListener('click', slideNext);
-    document.removeEventListener('keydown', nextSwap);
+    aboutBtnRefs.removeEventListener('focus', nextSwap);
   }
 });
 
 observAbout.observe(aboutRefs);
 
-function slideNext() {
-  aboutSwiper.slideNext();
-}
-
 function nextSwap(event) {
   event.preventDefault();
-  if (event.key === 'ArrowRight' || event.key === 'Tab') {
-    slideNext();
-  }
+  aboutSwiper.slideNext();
 }
