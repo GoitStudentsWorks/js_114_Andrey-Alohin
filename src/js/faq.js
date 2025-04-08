0c23e6b466831e305bb49e8410ad71a34ec2ffe6
Andrey-Alohin <andriealohin2020@outlook.com>
2025-04-08 22:50:45 +0300

    Small fixes

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,9 +1,15 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  new Accordion('.accordion-container', {
-    duration: 300,
-    showMultiple: true,
-  });
+new Accordion('.accordion-container', {
+  duration: 400,
+  showMultiple: true,
+  onOpen: function (currentElement) {
+    const icon = currentElement.querySelector('.faq-icon');
+    icon.classList.add('rotated');
+  },
+  onClose: function (currentElement) {
+    const icon = currentElement.querySelector('.faq-icon');
+    icon.classList.remove('rotated');
+  },
 });
